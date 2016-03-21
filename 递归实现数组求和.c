2649bd516ad33d69bbd1991373f8f41efdf29b20
helloworldzlg <helloworld_zlg@outlook.com>
2016-03-21 09:23:37 +0800

    格式调整

--- 递归实现数组求和.c
+++ 递归实现数组求和.c
@@ -2,14 +2,14 @@
 
 int Array_sum(int *a, int n)
 {   
-	printf("n = %d\n", n);
+    printf("n = %d\n", n);
 	 
     return (n > 0) ? Array_sum(a, n-1) + a[n-1] : 0;
 }
 
 int main()
 {
-	int i;
+    int i;
     int a[100] = {0};
     
     for (i = 0; i < sizeof(a)/sizeof(a[0]); i++)
